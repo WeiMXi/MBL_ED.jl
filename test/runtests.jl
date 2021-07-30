5f1d69aee8a9236033b7eea3d706e25fd83721ce
iYu <bai.a.y@qq.com>
2021-07-30 21:28:57 +0800

    try to increase code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,12 @@ using JLD
     , samples = 10
     )
     t_data = load("./test_data.jld")
-    r_t = cal_one_task(t_data["task"])
+    r1_t = cal_one_task(t_data["task"])
+    r2_t = primitive_run(tol_n = 10, up = div(10, 2), W = 1.0:0.5:2.0, samples = 10)
     @test tasks[2].H == t_data["task"].H
-    @test abs(r_t[:, 4][1] - t_data["result"][4]) < 0.01
-    @test abs(r_t[:, 5][1] - t_data["result"][5]) < 0.01
+    @test abs(r1_t[:, 4][1] - t_data["result"][4]) < 0.01
+    @test abs(r1_t[:, 5][1] - t_data["result"][5]) < 0.01
+    @test 0 < sum(r2_t.W)/length(r2_t.W) < Inf
+    @test 0 < sum(r2_t.r)/length(r2_t.r) < Inf
     # @test abs.( cal_one_task(t_data["task"])[4:5] .- t_data["result"][4:5] ) .< 0.01
 end
